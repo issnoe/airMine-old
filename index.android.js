2bd375b7a1bdc7d7033b21e26b3ec5d1b953d6b6
luisnoejasso <luisnoejasso@hotmail.com>
2017-06-15 03:16:33 -0500

    Nav calida del aire

--- index.android.js
+++ index.android.js
@@ -33,6 +33,21 @@ class airMine2 extends Component {
   }
 }
 class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {};
+    var options = {enableHighAccuracy: true, timeout: 10000, maximumAge: 3000}
+      navigator.geolocation.getCurrentPosition(function(pos){
+         // debugger;
+          //guardar en localStorage
+          this.setState(pos);
+          /**Mode init */
+      }.bind(this), function(error){
+          debugger;
+          //Actualizar localstorage
+      });
+  }
   // Nav options can be defined as a function of the screen's props:
   static navigationOptions = ({ navigation }) => ({
     title: `Chat with ${navigation.state.params.user}`,
@@ -41,27 +56,27 @@ class ChatScreen extends React.Component {
     // The screen's current route is passed in to `props.navigation.state`:
     const { params } = this.props.navigation.state;
     return (
-      <View>
-        <Text>Chat with {params.user}</Text>
-      </View>
+      <Inicio s={this.state}/>
     );
   }
 }
 
 class HomeScreen extends React.Component {
+  
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'AirMine - Red de datos',
     
   };
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>Hello, Chat App!</Text>
+         
         <Button
           onPress={() => navigate('Chat', { user: 'Lucy' })}
-          title="Chat with Lucy"
+          title="Calidad del Aire"
         />
+        
       </View>
     );
   }
