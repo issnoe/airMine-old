a895c04381c3e946d822d944b4d81090a54b2b3c
luisnoejasso <luisnoejasso@hotmail.com>
2017-08-02 11:03:15 -0500

    Init meteor

--- index.android.js
+++ index.android.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 
 import { AppRegistry, ActivityIndicator, ScrollView, ListView, Text, StyleSheet, View, Button, Image, TouchableOpacity } from 'react-native';
+
 //import TestComp from './assets/components/test'
 
 //import Inicio from './src/components/app.js'
@@ -18,6 +19,7 @@ import RankingScreen from './src/components/rankingList.js'
 import CreditosScreen from './src/components/creditos.js'
 
 import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation';
+console.log("hla mundo")
 const MyApp = DrawerNavigator({
   Usuario: {
     screen: UsuarioScreen
